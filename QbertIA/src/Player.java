a2438f0f4f629112dcf33e6a31b118aa61b7dffe
hunte <hunte@DESKTOP-QKEPBHC.homenet.telecomitalia.it>
2019-08-13 18:20:18 +0200

    Aggiunta classe player , definite variabili di istanza , e metodi

--- QbertIA/src/Player.java
+++ QbertIA/src/Player.java
@@ -1,4 +1,62 @@
 
+
 public class Player {
+	
+	public enum Status{U_RIGHT,D_RIGHT,U_LEFT,D_LEFT}
+	
+	private int life;
+	private boolean enemy;
+	private Status state=Status.D_LEFT;
+	
+	
+	public Player() {
+		life=3;
+		enemy=false;
+	}
+	
+	public Player(int life) {
+		this.life=life;
+		enemy=false;
+	}
+	
+	public Player(boolean enemy) {
+		this.enemy=enemy;
+		life=1;
+	}
+	
+	public Player(int life , boolean enemy) {
+		this.life=life;
+		this.enemy=enemy;
+	}
+	
+	public boolean isEnemy() {
+		return enemy;
+	}
+	
+	public int getLife() {
+		return life;
+	}
+	
+	public void setLife(int life) {
+		this.life=life;
+	}
+	
+	public int decrementLife() {
+		life--;
+		return life;
+	}
+	
+	public int incrementLife() {
+		life++;
+		return life;
+	}
+	
+	public Status getState() {
+		return state;
+	}
+	
+	public void setState(Status state) {
+		this.state=state;
+	}
 
 }

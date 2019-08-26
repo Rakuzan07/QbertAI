857d4ffc01ab38bb8122c17b50e014aeaebaa606
hunte <hunte@DESKTOP-QKEPBHC.homenet.telecomitalia.it>
2019-08-26 16:37:30 +0200

    Aggiunto hatch

--- QbertIA/src/logic/Snake.java
+++ QbertIA/src/logic/Snake.java
@@ -2,9 +2,20 @@ package logic;
 
 public class Snake extends Player{
 
+	public boolean egg;
+	
 	public Snake(int cont) {
 		super(cont);
         life=1;
+        egg=true;
+	}
+	
+	public void hatch() {
+		egg=false;
+	}
+	
+	public boolean getStatusHatch() {
+		return egg;
 	}
 
 }

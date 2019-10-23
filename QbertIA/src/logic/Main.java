9767d93248e342d33ab17ca11b57ee20d1846848
Zane83 <antony_97-@hotmail.it>
2019-10-23 16:30:02 +0200

    Fixato errore nell'ASP di Qbert

--- QbertIA/src/logic/Main.java
+++ QbertIA/src/logic/Main.java
@@ -4,8 +4,6 @@ import gui.QbertFrame;
 public class Main {
 	
 	public static void main(String...arg) {
-		//World w=new World();
-		//System.out.println(w);
 		QbertFrame qf=new QbertFrame();
 	}
 
